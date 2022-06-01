fc772ef273908e364a12c1e2837fbeac47abc7d3
vradugin <vradugin@phoenixit.ru>
2022-06-01 14:42:02 +0300

    Initial commit

---
+++ src/main/java/ru/example/olegich/testspringolegich/TestSpringOlegichApplication.java
@@ -0,0 +1,13 @@
+package ru.example.olegich.testspringolegich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringOlegichApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringOlegichApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/example/olegich/testspringolegich/TestSpringOlegichApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.olegich.testspringolegich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringOlegichApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
